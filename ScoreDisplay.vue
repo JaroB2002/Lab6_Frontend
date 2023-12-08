b408d43997704b2d809fb009ee22a3256e495e7d
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-12-08 17:34:50 +0100

    Feat: basic styling ScoreDisplay

---
+++ ScoreDisplay.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h1>Live Scoreboard</h1>
+      <ul>
+        <li v-for="item in data" :key="item.rider">
+          <span>{{ item.rider }}</span>
+          <span>{{ item.score }}</span>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const data = ref([
+    { "rider": "Wout Van Aert", "score": 31 },
+    { "rider": "Mathieu Van Der Poel", "score": 1 },
+    { "rider": "Eli Iserbyt", "score": 10 },
+    { "rider": "Laurens Sweeck", "score": 8 },
+  ]);
+  </script>
+  
